762a08d2bd5b07e06cefa97a60e26094493f568f
Ashok <ashokdampana@gmail.com>
2026-03-13 18:25:28 +0530

    added  console.log()  to verify  db url   working

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
+    console.log("URI Length:", process.env.MONGO_URI ? process.env.MONGO_URI.length : "UNDEFINED");
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log('=> DB Connected...');
   } catch (error) {
